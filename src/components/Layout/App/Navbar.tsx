2839830b0e604f8783988d69000598ae60e6ef4b
Cyril de Wit <cyrilwit@gmail.com>
2023-12-01 12:56:51 +0100

    feat: improve skilltree

---
+++ src/components/Layout/App/Navbar.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Avatar, Dropdown, Navbar as FlowbiteNavbar } from 'flowbite-react';
+import cyrildewitLogo from '@/assets/16477999.png'
+
+export default function Navbar() {
+    return (
+        <FlowbiteNavbar fluid rounded className={"shadow"}>
+            <FlowbiteNavbar.Brand href="https://flowbite-react.com">
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">SkillFlow</span>
+            </FlowbiteNavbar.Brand>
+
+            <div className="flex md:order-2">
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                        <Avatar alt="User settings" img="https://avatars.githubusercontent.com/u/16477999?v=4" rounded />
+                    }
+                >
+                    <Dropdown.Header>
+                        <span className="block text-sm">Cyril de Wit</span>
+                        <span className="block truncate text-sm font-medium">Fontys Hogeschool</span>
+                    </Dropdown.Header>
+                    {/*<Dropdown.Item>Settings</Dropdown.Item>*/}
+                    {/*<Dropdown.Divider />*/}
+                    <Dropdown.Item>Uitloggen</Dropdown.Item>
+                </Dropdown>
+                <FlowbiteNavbar.Toggle />
+            </div>
+            <FlowbiteNavbar.Collapse>
+                <FlowbiteNavbar.Link href="#" active>
+                    Dashboard
+                </FlowbiteNavbar.Link>
+            </FlowbiteNavbar.Collapse>
+        </FlowbiteNavbar>
+    );
+}
+
+
