2656cc6448e1df3ad2b5dd6f4e69c6b152b198f5
Cyril de Wit <cyrilwit@gmail.com>
2023-12-17 13:50:42 +0100

    feat: replaces classes with async functions

--- src/api/repositories/course-repository.ts
+++ src/api/repositories/course-repository.ts
@@ -1,34 +1,32 @@
+'use server'
+
 import { api, getBearerToken, getAuthenticatedUserUuid } from '@/api/api-client'
 
-export class CourseRepository {
+export const getUserEnrolledCourses = async () => {
+    const userUuid = await getAuthenticatedUserUuid()
 
-    static async getUserEnrolledCourses() {
-        const userUuid = await getAuthenticatedUserUuid()
-
-        const config = {
-            headers: {
-                Authorization: await getBearerToken()
-            },
-            params: {
-                sort: '-created_at',
-            },
-        }
-
-        const response = await api.get(`users/${userUuid}/enrolled_courses`, config)
-
-        return response?.data?.data
+    const config = {
+        headers: {
+            Authorization: await getBearerToken()
+        },
+        params: {
+            sort: '-created_at',
+        },
     }
 
-    static async findByUuid({ courseUuid }) {
-        const config = {
-            headers: {
-                Authorization: await getBearerToken()
-            },
-        }
+    const response = await api.get(`users/${userUuid}/enrolled_courses`, config)
 
-        const response = await api.get(`courses/${courseUuid}`, config)
+    return response?.data?.data
+}
 
-        return response?.data?.data
+export const findByUuid = async ({ courseUuid }) => {
+    const config = {
+        headers: {
+            Authorization: await getBearerToken()
+        },
     }
 
+    const response = await api.get(`courses/${courseUuid}`, config)
+
+    return response?.data?.data
 }
--- src/api/repositories/skill-repository.ts
+++ src/api/repositories/skill-repository.ts
@@ -1,17 +1,27 @@
+'use server'
+
 import { api, getBearerToken } from '@/api/api-client'
 
-export class SkillRepository {
-
-    static async getSkilltree() {
-        const config = {
-            headers: {
-                Authorization: await getBearerToken()
-            },
-        }
-
-        const response = await api.get(`skilltrees/1`, config)
-
-        return response?.data?.data
+export const getSkilltree = async () => {
+    const config = {
+        headers: {
+            Authorization: await getBearerToken()
+        },
     }
 
+    const response = await api.get(`skilltrees/1`, config)
+
+    return response?.data?.data
+}
+
+export const getSkill = async (uuid) => {
+    const config = {
+        headers: {
+            Authorization: await getBearerToken()
+        },
+    }
+
+    const response = await api.get(`skills/${uuid}`, config)
+
+    return response?.data?.data
 }
